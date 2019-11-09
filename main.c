0db4a25317d7b475eda88da5962c4db01d7ffcea
eleft <eleft@protonmail.com>
2019-11-09 17:54:11 +0200

    Fixed segfault error

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
 #include"board.h"
+#include<stdlib.h>
 
 int main(){
 
     Board* board;
+    board = (Board*) malloc(sizeof(Board));
     printf("%d",InitBoard(board, 5, 6));
 
     return 0;
