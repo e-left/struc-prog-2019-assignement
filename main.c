ce5fe4f7b4eb66a06a708d73509c9e1c720fdf04
Manos Tsardoulias <etsardou@gmail.com>
2019-11-24 13:00:36 +0200

    Visualization and gameplay changes

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
+#include <unistd.h>
 
 #include"board.h"
 
@@ -15,26 +16,39 @@ int (*player2)(const Board* b) = random_player;
 int (*player1)(const Board* b) = random_player;
 
 int main(){
+    // Must be called one time
+    srand(time(0));
 
     Board* board;
     board = (Board*) malloc(sizeof(Board));
     int width, height;
+    float delay = 0;
+    int moves = 0;
 
 
     printf("Hello and welcome to the advanced connect4 game!\n");
     printf("You know the rules, you know the game, you (probably) have a working solution.\n");
     printf("Input the dimensions(rows x columns): ");
     scanf("%d %d", &height, &width);
+    printf("Input the delay in seconds between plays (0 if you do not want delay and -1 if you want to press enter to continue): ");
+    scanf("%f", &delay);
     printf("Let the game begin!\n");
     InitBoard(board, height, width);
 
     while(!CheckFull(board)){
+        if(delay < 0){
+            printf("Press enter to continue...");
+            getchar();
+        }
+        else usleep(delay * 1000000);
+
+        printf("\n-------------- Move #%d --------------\n", moves++);
         int move_a = player1(board);
 
         if(CheckMove(board, move_a)){
             printf("Player 1 Loses. Player 2 wins!(invalid move)\n");
             PrintBoard(board);
-            break;
+            exit(0);
         }
         InsertInto(board, move_a, 1);
         PrintBoard(board);
@@ -42,7 +56,7 @@ int main(){
         if(CheckMove(board, move_b)){
             printf("Player 2 Loses. Player 1 wins!(invalid move)\n");
             PrintBoard(board);
-            break;
+            exit(0);
         }
         InsertInto(board, move_b, 2);
         PrintBoard(board);
--- player_random.c
+++ player_random.c
@@ -12,6 +12,5 @@ extern int random_player(const Board* b){
     }
 
     int move = rand() % (counter + 1);
-
     return moves[move];
 }
